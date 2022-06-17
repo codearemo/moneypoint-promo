c96951c326d1c12772f6c2cd00f3406102f81fd3
Busola <busola.omosipe@gmail.com>
2022-06-17 08:59:18 +0100

    Changed Count down picture to HTML

--- script/main.js
+++ script/main.js
@@ -3,10 +3,8 @@ const main = document.querySelector('main');
 
 setTimeout(() => {
   main.classList.add('show-main');
-}, 900);
-
-setTimeout(() => {
   hiddenComponent.forEach(component => {
     component.classList.remove('hide');
   });
-}, 2000);
+}, 100);
+
